61b74313099c0bb6f084692be09d8abfca270c4f
蒋勇 <jiangy@highzap.com>
2022-09-14 19:14:14 +0800

    chore: build

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 npm run build
-
-git add . && git commit -m 'chore: build'
-
+git add .
+git commit -m 'chore: build'
 npm run release && git push --follow-tags origin master && npm publish
