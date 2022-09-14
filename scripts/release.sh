f4aab8d937bf5b9f42f09dcedc4b4a5d7f44d408
蒋勇 <jiangy@highzap.com>
2022-09-14 19:08:30 +0800

    chore: update scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run build && npm run release && git push --follow-tags origin master && npm publish
+npm run build && git add . && git commit -m 'chore: build' && npm run release && git push --follow-tags origin master && npm publish
